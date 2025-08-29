1ba2e4ef3fa58f8b66437336e903cab6049fb264
ttchef <ttchef@github.com>
2025-08-29 13:43:14 +0200

    adds block structure and map click detection

--- main.c
+++ main.c
@@ -11,8 +11,17 @@ const uint32_t screenWidht = 12;
 const uint32_t screenHeight = 15;
 const uint32_t mapWidth = 6; 
 const uint32_t mapHeight = 10;
+const uint32_t mapPosX = 3;
+const uint32_t mapPosY = 2;
 
-void drawMap(const uint32_t mapPosX, const uint32_t mapPosY) {
+typedef struct {
+    const uint32_t posX; // Relative to map
+    const uint32_t posY;
+    const uint32_t value;
+    Color color;
+} Block;
+
+void drawMap() {
     uint32_t k = 0;
     for (uint32_t i = 0; i < mapWidth; i++) {
         for (uint32_t j = 0; j < mapHeight; j++) {
@@ -26,17 +35,37 @@ void drawMap(const uint32_t mapPosX, const uint32_t mapPosY) {
     }
 }
 
+void drawBlock(Block* block) {
+    DrawRectangle((block->posX + mapPosX) * GRIDSIZE, (block->posY + mapPosY) * GRIDSIZE, GRIDSIZE, GRIDSIZE, block->color);
+}
+
 int main() {
 
     InitWindow(screenWidht * GRIDSIZE, screenHeight * GRIDSIZE, "Mef 2048");
     SetTargetFPS(60);
+    
+    //Block blocks[mapWidth * mapHeight];
 
     while (!WindowShouldClose()) {
 
+        // Input 
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+            Vector2 mousePos = GetMousePosition();
+           
+            if (!(mousePos.x < mapPosX * GRIDSIZE || 
+                mousePos.x > (mapPosX + mapWidth) * GRIDSIZE || 
+                mousePos.y < mapPosY * GRIDSIZE || 
+                mousePos.y > (mapPosY + mapHeight) * GRIDSIZE)) {
+                printf("Mouse Pos: %f  |  %f\n", mousePos.x, mousePos.y);
+            }
+        }
+    
+
+        // Render
         BeginDrawing();
 
-        drawMap(3, 2);
-    
+        drawMap();
+            
         EndDrawing();
 
     }
