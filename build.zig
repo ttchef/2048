3545213393ce9479cd9e1202639374b48fb7310d
HaraldWik <wikhallharald@gmail.com>
2025-08-30 21:35:08 +0200

    Ported to zig build system

---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const raylib = b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "2048",
+        .root_module = b.addTranslateC(.{
+            .root_source_file = b.path("src/main.c"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }).createModule(),
+    });
+    exe.linkLibrary(raylib.artifact("raylib"));
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| run_cmd.addArgs(args);
+}
